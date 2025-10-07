70b748dcb5455dd377a091c35fd78ff14fbb0674
Nikita <lindendnns@yahoo.com>
2025-10-07 09:46:14 +0300

    Removed unnecessary files

--- backend/runner/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('run-checks/', views.run_checks_view, name='run_checks'),
-    path('reports/', views.get_reports_list, name='reports_list'),
-    path('reports/<int:report_id>/json/', views.get_report_json, name='report_json'),
-]
