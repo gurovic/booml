7593305fc92b1560ffea18cf540e736867e83562
Степан Дворников <stepandv@Stepans-MacBook-Air.local>
2025-10-09 22:40:04 +0300

    Delete task model

--- backend/runner/models.py
+++ backend/runner/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    data = models.CharField(max_length=200, blank=True, null=True)
-
-    def __str__(self):
-        return self.title
