664ad996dea859b532b8440aef215f9bf8d1e2b5
dariaporay <daryaporay@gmail.com>
2025-10-13 02:46:10 +0300

    add run_code

--- backend/runner/urls.py
+++ backend/runner/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views.main_page import main_page
 from .views.authorization import register_view, login_view, home_view
+from .views.run_code import run_code
+
+
+app_name = 'runner'
 
 urlpatterns = [
     path("", main_page, name="task_list"),
     path('register/', register_view, name='register'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('run/', run_code, name='run_code')
 ]
 
