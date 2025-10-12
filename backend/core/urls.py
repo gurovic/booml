eebf79a0aa0545fae2232b27375d566cd21a7cf7
Nikita <lindendnns@yahoo.com>
2025-10-12 23:06:37 +0300

    Updated urls in core folder

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('checker/', include('runner.urls')),  # Добавляем маршруты чекера
+    path('runner/', include('runner.urls'))  # Добавляем маршруты чекера
 ]
