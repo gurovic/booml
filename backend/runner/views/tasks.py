a1f98eb992a92dae8d3adb0b1dc3c9eb9dc45126
Nikita Shilov <st1pchikyt@gmail.com>
2025-10-12 10:36:27 +0300

    Убран CSS из шаблона, заменены названия переменных, убрано условие из таблицы с задачами

--- backend/runner/views/tasks.py
+++ backend/runner/views/tasks.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator
 from ..models import Task
 
 def task_list(request):
-    qs = Task.objects.only("title", "statement", "created_at").order_by("-created_at")
-    paginator = Paginator(qs, 50)
+    tasks = Task.objects.only("title", "created_at").order_by("-created_at")
+    paginator = Paginator(tasks, 50)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     return render(request, "runner/task_list.html", {"page_obj": page_obj})
