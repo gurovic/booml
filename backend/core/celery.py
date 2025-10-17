656ab9b7b1cb36640ab841d7d884fdb2378e759b
AnayLu <uliana_shur@mail.ru>
2025-10-17 10:26:13 +0300

    fixed bugs

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -1,8 +1,1 @@
-import os
-from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booml.settings")
-
-app = Celery("booml")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
