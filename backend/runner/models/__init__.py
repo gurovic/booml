f4105af7cf80fb4d9977dcdeb0a1b5e3018c7a6b
Nikita <lindendnns@yahoo.com>
2025-11-06 04:54:19 +0300

    Added tests for checker and metrics files

--- backend/runner/models/__init__.py
+++ backend/runner/models/__init__.py
@@ -5,7 +5,7 @@ from .report import Report
 from .submission import Submission
 from .contest import Contest
 #from .problem_desriptor import ProblemDescriptor
-from .submission_pre_validation import SubmissionPreValidation
+from .prevalidation import PreValidation 
 from .leaderboard import Leaderboard
 from .notebook import Notebook
 from .task import Task as Problem
