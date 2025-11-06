e6874f805e349224242ba4d7b2d6cc18a4527d28
Степан Дворников <stepandv@Stepans-MacBook-Air.local>
2025-11-07 01:19:49 +0300

    Added Markdown rendering

--- backend/runner/models/contest.py
+++ backend/runner/models/contest.py
@@ -3,22 +3,22 @@ from django.db.models import CharField, IntegerField, DateField, ManyToManyField
 
 class Contest(models.Model):
   problems = ManyToManyField("Problem", blank=True)
-  source = CharField()
+  source = CharField(max_length=255)
   _type = IntegerField(db_column="type", blank=True)
   difficulty = IntegerField(blank=True)
   start_time = DateField()
   duration = IntegerField(blank=True)
-  
-  STATUS_CHOICES = {
-    0: "going",
-    1: "after-solving"
-  }
+
+  STATUS_CHOICES = [
+      (0, "going"),
+      (1, "after-solving"),
+  ]
   status = IntegerField(choices=STATUS_CHOICES, default=0)
-  
-  OPEN_CHOICES = {
-    0: "closed",
-    1: "opened"
-  }
+
+  OPEN_CHOICES = [
+      (0, "closed"),
+      (1, "opened"),
+  ]
   open = IntegerField(default=0, choices=OPEN_CHOICES)
   
   leaderBoard = models.ForeignKey("Leaderboard", on_delete=models.CASCADE)
