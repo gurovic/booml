cd6838741f6d2efb74a647194866e30b322808d6
Nikita <lindendnns@yahoo.com>
2025-11-06 04:16:54 +0300

    Removed unnecessary line

--- backend/runner/models/task.py
+++ backend/runner/models/task.py
@@ -7,7 +7,6 @@ class Task(models.Model):
     statement = models.TextField("Условие", blank=True)
     created_at = models.DateField("Дата создания", blank=True)
     rating = models.IntegerField("Рейтинг сложности", validators=[MinValueValidator(800), MaxValueValidator(3000)], default=800)
-    ground_truth_file = models.FileField("Файл с эталонными ответами", upload_to="tasks/ground_truth/", null=True, blank=True)
 
     def __str__(self):
         return self.title
