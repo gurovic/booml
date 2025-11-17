8572eaf7fba47ef4d06434e59e02952cbb116e6d
Jonhef <jonhefmine@gmail.com>
2025-11-17 13:13:44 +0300

    Add celery[redis] into requirements, add celery file for settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -132,5 +132,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/1"
+
+CELERY_TASK_ALWAYS_EAGER = False  # для реального async
+CELERY_TASK_EAGER_PROPAGATES = True
