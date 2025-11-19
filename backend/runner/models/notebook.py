a993c23c9e0ebf1d6e035c29c628321250d8fca9
AnayLu <uliana_shur@mail.ru>
2025-11-19 22:29:40 +0300

    изменение доступа к notebook

--- backend/runner/models/notebook.py
+++ backend/runner/models/notebook.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-
+User = get_user_model()
 class Notebook(models.Model):
     owner = models.ForeignKey(
         User,
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
         related_name='notebooks',
-        null=True,
-        blank=True,
     )
     title = models.CharField(max_length=200, default="Новый блокнот")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
