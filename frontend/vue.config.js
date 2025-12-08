fe849ae6cf0003bd8f7b3baa5e1a8b9c091741ab
Nikita Shilov <st1pchikyt@gmail.com>
2025-12-08 19:35:55 +0300

    Исправлен конфиг фронтенда

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    allowedHosts: "all",
     port: 8101,
     host: "0.0.0.0",
     proxy: {
