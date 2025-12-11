7b1a37fe905ef9dcfec80c0877fb7acc8a94897e
GregoryNosich <gregorynosv@gmail.com>
2025-12-11 21:38:11 +0300

    Add UI-component link list

---
+++ frontend/src/components/ui/UiLinkList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu-list">
+    <h1>{{ title }}</h1>
+
+    <ul>
+      <li v-for="item in items" :key="item.text">
+        <router-link :to="item.route">
+          {{ item.text }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuList",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
