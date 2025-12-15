29805280f9618fa7abf6c1b124dcfbe85b4f4310
JD <klim.i.rumyantsev@gmail.com>
2025-12-15 17:32:59 +0300

    Fixed 2

---
+++ backend/runner/migrations/0015_merge_202503XX_0000.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("runner", "0014_contest_moderation_fields"),
+        ("runner", "0014_hierarchy_rewrite"),
+    ]
+
+    operations = []
