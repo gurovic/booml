d6e729d63d4538dda3d01ac804cece89cf3937a7
Jonhef <jonhefmine@gmail.com>
2025-12-22 15:32:36 +0300

    Return js

--- backend/runner/static/notebook/js/notebook_detail.js
+++ backend/runner/static/notebook/js/notebook_detail.js
@@ -1993,18 +1993,6 @@ const notebookDetail = {
     }
 
 };
-document.addEventListener('DOMContentLoaded', function() {
-    notebookDetail.init({
-        notebookId: {{ notebook.id }},
-        csrfToken: '{{ csrf_token }}',
-        runCellUrl: '{% url "run-cell" %}',
-        sessionCreateUrl: '{% url "notebook-session-create" %}',
-        sessionResetUrl: '{% url "session-reset" %}',
-        sessionFilesUrl: '{% url "session-files" %}',
-        sessionFileDownloadUrl: '{% url "session-file-download" %}',
-        sessionStopUrl: '{% url "session-stop" %}'
-    });
-});
 const runAll = function() {
     const cells = document.querySelectorAll('.cell')
     for (const cell of cells) {
