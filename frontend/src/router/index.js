67b76d69f10536bfee053c8fc97143a63f663d88
GregoryNosich <gregorynosv@gmail.com>
2025-12-25 23:17:48 +0400

    add contest page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import ProblemPage from '@/pages/ProblemPage.vue'
 import HomePage from '@/pages/HomePage.vue'
 import SectionPage from '@/pages/SectionPage.vue'
 import CoursePage from '@/pages/CoursePage.vue'
+import ContestPage from '@/pages/ContestPage.vue'
 import LoginPage from '@/pages/LoginPage.vue'
 import RegisterPage from '@/pages/RegisterPage.vue'
 
@@ -34,6 +35,11 @@ const router = createRouter({
       path: '/course/:id',
       name: 'course',
       component: CoursePage,
+    },
+    {
+      path: '/contest/:id',
+      name: 'contest',
+      component: ContestPage,
     },{
       path: '/login',
       name: 'login',
