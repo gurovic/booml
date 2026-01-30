27609d9cdcbbef1a83ebea0de6520608d2faaca4
Nikita Shilov <st1pchikyt@gmail.com>
2026-01-30 12:21:20 +0300

    Добавлен подвал

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <router-view />
+  <div class="app">
+    <main class="app-content">
+      <router-view />
+    </main>
+    <UiFooter />
+  </div>
 </template>
 
 <script setup>
+import UiFooter from '@/components/ui/UiFooter.vue'
 </script>
 
 <style scoped>
